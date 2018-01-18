c290de931bc6853b8435b5035d598fbb02fbc5dd
rkdk12 <kjg@mz.co.kr>
2018-01-18 09:49:12 +0900

    add nickname

--- server.js
+++ server.js
@@ -12,11 +12,26 @@ http.listen(port, function(){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+  // socket.on('chat message', function(msg){
+  //   io.emit('chat message', msg);
+  // });
   // io.emit('chat message', 'a user connected');
-  socket.on('disconnect', function(){
-    //io.emit('chat message', 'user disconnected');
+  socket.on('disconnect', function(data){
+    io.emit('disconnect', data);
+    console.log('exit users' + data);
   });
+  socket.on('connect', function(data){
+    io.emit('connect', data);
+    console.log('connect users' + data);
+  });
+    // Join Room
+    socket.on('join', function(data) {
+      socket.join('room' + data.roomId);
+      console.log('join users' + data);
+    });
+    // Broadcast to room
+    socket.on('chat message', function(data) {
+      io.sockets.in('room' + data.roomId).emit('chat message', data.message);
+      console.log('chat message' + data);
+    });
 });
