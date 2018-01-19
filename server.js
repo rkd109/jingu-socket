4f72acfefd3047006f9f88e3d186d3cf00e3a653
rkdk12 <kjg@mz.co.kr>
2018-01-19 14:09:51 +0900

    gitgitgit...

--- server.js
+++ server.js
@@ -4,31 +4,26 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var port = process.env.PORT || 8080;
-// var publicPath = path.resolve(__dirname, 'www');
-// var path = require('path');
+
+io.set('heartbeat timeout', 50000);
+io.set('heartbeat interval', 10000);
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 app.use(express.static(__dirname + '/public'));
-console.log(__dirname);
 
 http.listen(port, function(){
   console.log('listening on *:'+port);
 });
 
 io.on('connection', function(socket){
-  // socket.on('chat message', function(msg){
-  //   io.emit('chat message', msg);
-  // });
-  // io.emit('chat message', 'a user connected');
-  var username ;
-  var roomname;
-  socket.on('disconnect', function(data){
-    // io.emit('disconnect', name);
-    io.sockets.in('room' + roomname).emit('disconnect', username);
-    
-    console.log('exit users' + username);
+    var username ;
+    var roomname;
+    socket.on('disconnect', function(data){
+        io.sockets.in('room' + roomname).emit('disconnect', username);
+        console.log('exit users' + username);
   });
   // socket.on('connect', function(data){
   //   io.emit('connect', data);
@@ -36,15 +31,15 @@ io.on('connection', function(socket){
   // });
     // Join Room 
     socket.on('join', function(data) {
-      socket.join('room' + data.roomId);
-      username = data.nickName;
-      roomname = data.roomname;
-      io.sockets.in('room' + data.roomname).emit('join message', data.nickName);
-      console.log('join users : ' + data.nickName);
+        socket.join('room' + data.roomId);
+        username = data.nickName;
+        roomname = data.roomname;
+        io.sockets.in('room' + data.roomname).emit('join message', data.nickName);
+        console.log('join users : ' + data.nickName);
     });
     // Broadcast to room
     socket.on('chat message', function(data) {
-      io.sockets.in('room' + data.roomname).emit('chat message', data.message);
-      console.log('chat message : ' + data.message);
+        io.sockets.in('room' + data.roomname).emit('chat message', data.message);
+        console.log('chat message : ' + data.message);
     });
 });  
